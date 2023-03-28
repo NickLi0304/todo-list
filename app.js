e56b49e361c52040a67825b68a6f263eea7b721d
@NickLi0304 <twinpeterlee@gmail.com>
2023-03-29 03:03:17 +0800

    refactor: modify mongoose config

--- app.js
+++ app.js
@@ -1,5 +1,4 @@
 const express = require("express");
-const mongoose = require("mongoose"); // 載入 mongoose
 const exphbs = require("express-handlebars");
 const bodyParser = require("body-parser");
 const methodOverride = require("method-override");
@@ -9,7 +8,7 @@ const Todo = require("./models/todo"); // 載入 Todo model
 
 // 引用路由器
 const routes = require('./routes')
-
+require("./config/mongoose");
 
 //僅在非正式環境時，使用 dotenv
 if (process.env.NODE_ENV !== "production") {
@@ -22,34 +21,10 @@ const app = express();
 app.engine("hbs", exphbs({ defaultLayout: "main", extname: ".hbs" }));
 app.set("view engine", "hbs");
 
-mongoose.connect(process.env.MONGODB_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-}); //設定連線到mongoDB
-
-// 取得資料庫連線狀態
-const db = mongoose.connection;
-// 連線異常
-db.on("error", () => {
-  console.log("mongodb error!");
-});
-// 連線成功
-db.once("open", () => {
-  console.log("mongodb connected!");
-});
-
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(methodOverride("_method"));
 app.use(routes)
-
-// app.get("/", (req, res) => {
-//   Todo.find() // 取出 Todo model 裡的所有資料
-//     .lean() // 把 Mongoose 的 Model 物件轉換成乾淨的 JavaScript 資料陣列
-//     .sort({ _id: "asc" }) //根據 _id 升冪排序
-//     .then((todos) => res.render("index", { todos })) // 將資料傳給 index 樣板
-//     .catch((error) => console.error(error));
-// });
 
 
 app.listen(3000, () => {
